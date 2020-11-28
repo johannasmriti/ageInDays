98c8e604210eee5ae56e9b45fc5a92d33246d18b
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2020-11-28 07:34:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//Age in Days
+function ageInDays(){
+    var birthYear = prompt('What year where you born?!');
+    var ageInD = (2020-birthYear)*365;
+    var h1= document.createElement('h1');
+    var textAnswer = document.createTextNode('You are '+ageInD+" days old.");
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
